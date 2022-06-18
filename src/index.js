eac44b5e6091222a65a4fc99a178c4923a406d27
ashhhlynn <ashleylynnwelch@gmail.com>
2022-06-18 01:18:25 -0500

    commit tree e19d08297d0a9a55f0f19c5713558b8b321e6270

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+
+
+fetch(const imgUrl = "https://dog.ceo/api/breeds/image/random/4")
+
+
+.then(function(response) {
+  console.log(response);
+  return response.json();
+})
+.then(function(json) {
+  console.log(json);
+});
